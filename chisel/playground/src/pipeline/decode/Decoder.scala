5c2305b0eda13a1703db02ef9388bc640da12ae5
Felix <xi.lifeng@qq.com>
2025-03-17 22:42:32 +0800

    feat(Decoder): 增加代码提示，降低实验难度

--- chisel/playground/src/pipeline/decode/Decoder.scala
+++ chisel/playground/src/pipeline/decode/Decoder.scala
@@ -16,5 +16,20 @@ class Decoder extends Module with HasInstrType {
       val info = new Info()
     })
   })
+
+  val inst = io.in.inst
+  // 根据输入的指令inst从Instructions.DecodeTable中查找对应的指令类型、功能单元类型和功能单元操作类型
+  // 如果找不到匹配的指令，则使用Instructions.DecodeDefault作为默认值
+  val instrType :: fuType :: fuOpType :: Nil =
+    ListLookup(inst, Instructions.DecodeDefault, Instructions.DecodeTable)
+
+  val (rs, rt, rd) = (inst(19, 15), inst(24, 20), inst(11, 7))
+
   // TODO: 完成Decoder模块的逻辑
+  // io.out.info.valid      :=
+  // io.out.info.src1_raddr :=
+  // io.out.info.src2_raddr :=
+  // io.out.info.op         :=
+  // io.out.info.reg_wen    :=
+  // io.out.info.reg_waddr  :=
 }
